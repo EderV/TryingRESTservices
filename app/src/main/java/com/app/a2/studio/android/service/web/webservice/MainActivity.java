7850ac60c8b4472cabadee859c6c0bea159034ee
EderV <EderV@github.com>
2018-07-16 18:18:57 +0200

    Initial commit

---
+++ app/src/main/java/com/app/a2/studio/android/service/web/webservice/MainActivity.java
@@ -0,0 +1,135 @@
+package com.app.a2.studio.android.service.web.webservice;
+
+import android.app.VoiceInteractor;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView, textView2;
+
+    String USERNAME = "a";
+    String PASSWORD = "x";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.text);
+        textView2 = (TextView) findViewById(R.id.textView);
+
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+        textView2.setText(String.valueOf(isConnected));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v.getId() == R.id.button){
+                    WebService1 webService1 = new WebService1();
+                    webService1.execute();
+                }
+            }
+        });
+
+    }
+
+    private class WebService1 extends AsyncTask<Void, Void, Void>{
+
+        String USERNAME = "a";
+        String PASSWORD = "x";
+
+        String url = "http://192.168.1.39:8081/persona/hola";
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+            /* ******************* */
+            JsonObjectRequest objectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    url,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            textView.setText("Response: " + response.toString());
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            textView.setText("Error: " + error.toString());
+                        }
+                    }
+
+            ){
+
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    HashMap<String, String> headers = new HashMap<String, String>();
+
+                    String credentials = USERNAME+":"+PASSWORD;
+                    String auth = "Basic " + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+                    headers.put("Authorization", auth);
+                    return headers;
+                }};
+
+            /* ******************* */
+
+            requestQueue.add(objectRequest);
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+    }
+}
+
+/*
+
+
+
+*/
+
+
